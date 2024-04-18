851ba2a46d238593e5efa48f58e1bce096a9229c
seanMooney1996 <seanmooney1996@gmail.com>
2024-04-18 20:49:04 +0100

    fixed some errors causing crashes with splitter bug

--- cppProjectCA2/source/Splitter.cpp
+++ cppProjectCA2/source/Splitter.cpp
@@ -5,14 +5,15 @@
 #include "../headers/Splitter.h"
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 string Splitter::getBugDetails() {
     string bugDetails;
     bugDetails = to_string(id) + " SPLITTER (" + to_string(position.getX()) + "," + to_string(position.getY()) +
-                 ") " + to_string(size) +" "+ directionToString(
-            direction)+  " "+ to_string(splitFrequency);
+                 ") " + to_string(size) + " " + directionToString(
+            direction) + " " + to_string(splitFrequency);
 
-    if (alive){
+    if (alive) {
         bugDetails += " ALIVE ";
     } else {
         bugDetails += " DEAD ";
@@ -22,23 +23,58 @@ string Splitter::getBugDetails() {
 }
 
 void Splitter::displayBug() {
-    cout<<getBugDetails()<<endl;
+    cout << getBugDetails() << endl;
 }
 
 // when splitter splits, its frequency for splitting is decreased( multiplied by 2)
 void Splitter::split() {
 
-    if (size%2!=0){
-        size-=1;
+    if (size % 2 != 0) {
+        size -= 1;
     }
-    size = size/2;
-    auto *newSplitter = new Splitter(id+size+this->getPosition().getX(), this->getPosition().getX(), this->getPosition().getY(),
-                                     direction, size, splitFrequency * 2,board);
-    travelDistance =0;
+    this->size = size / 2;
 
-    cout<<id<<" has SPLIT!"<< endl;
+    auto *newSplitter = new Splitter(id + size + this->getPosition().getX(), this->getPosition().getX(),
+                                     this->getPosition().getY(),
+                                     direction, size, splitFrequency * 2, board);
+    this->travelDistance = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::vector<Direction> directions = {Direction::North, Direction::South, Direction::East, Direction::West};
+    std::shuffle(directions.begin(), directions.end(), gen);
+
+
+    for (Direction dir: directions) {
+        if (dir != this->direction && !newSplitter->isWayBlocked()) {
+            newSplitter->direction = dir;
+            break;
+        }
+    }
+
+    cout << id << " has SPLIT!" << endl;
     // move the new splitter away from the original
-    do {
+
+    if (newSplitter->direction == Direction::North) {
+        newSplitter->position.setX((newSplitter->position.getX()));
+        cout << newSplitter->id << " id" << endl;
+        cout << " y: " << newSplitter->getPosition().getY() << " x: " << newSplitter->getPosition().getX() << endl;
+    } else if (newSplitter->direction == Direction::South) {
+        newSplitter->position.setX((newSplitter->position.getX()));
+        cout << " y: " << newSplitter->getPosition().getY() << " x: " << newSplitter->getPosition().getX() << endl;
+    } else if (newSplitter->direction == Direction::East) {
+        newSplitter->position.setY((newSplitter->position.getY()));
+        cout << " y: " << newSplitter->getPosition().getY() << " x: " << newSplitter->getPosition().getX() << endl;
+    } else if (newSplitter->direction == Direction::West) {
+        newSplitter->position.setY((newSplitter->position.getY()));
+        cout << " y: " << newSplitter->getPosition().getY() << " x: " << newSplitter->getPosition().getX() << endl;
+    }
+    board->addNewBugToBoard(newSplitter);
+}
+
+void Splitter::move() {
+
+
+    while (this->isWayBlocked()) {
         std::random_device rd;
         std::mt19937 gen(rd());
         int min = 1;
@@ -47,7 +83,7 @@ void Splitter::split() {
         int randomInt = dist(gen);
 
         if (randomInt == 1) {
-            this->direction =  Direction::North;
+            this->direction = Direction::North;
         } else if (randomInt == 2) {
             this->direction = Direction::South;
         } else if (randomInt == 3) {
@@ -55,71 +91,39 @@ void Splitter::split() {
         } else if (randomInt == 4) {
             this->direction = Direction::West;
         }
-    } while (newSplitter->isWayBlocked() && this->getDirection() == newSplitter->getDirection());
-
-    if (this->direction ==  Direction::North) {
-        this->position.setX((this->position.getX())-1);
-    } else if (this->direction == Direction::South) {
-        this->position.setX((this->position.getX())+1);
-    } else if (this->direction == Direction::East) {
-        this->position.setY((this->position.getY())+1);
-    } else if (this->direction == Direction::West) {
-        this->position.setY((this->position.getY())-1);
     }
-    board->addNewBugToBoard(newSplitter);
-}
 
-void Splitter::move() {
-    // if the travel distance is divisible by the split frequency, and it's size divided by two doesn't leave a remainder
+    if (this->direction == Direction::North) {
+        this->position.setX((this->position.getX()) - 1);
+    } else if (this->direction == Direction::South) {
+        this->position.setX((this->position.getX()) + 1);
+    } else if (this->direction == Direction::East) {
+        this->position.setY((this->position.getY()) + 1);
+    } else if (this->direction == Direction::West) {
+        this->position.setY((this->position.getY()) - 1);
+    }
+
+    // if the travel distance is divisible by the split frequency, and size is greater than or equal to 4
     if (travelDistance==splitFrequency && size>=4) {
         split();
-    } else {
-
-        while (this->isWayBlocked()) {
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            int min = 1;
-            int max = 4;
-            std::uniform_int_distribution<> dist(min, max);
-            int randomInt = dist(gen);
-
-            if (randomInt == 1) {
-                this->direction = Direction::North;
-            } else if (randomInt == 2) {
-                this->direction = Direction::South;
-            } else if (randomInt == 3) {
-                this->direction = Direction::East;
-            } else if (randomInt == 4) {
-                this->direction = Direction::West;
-            }
-        }
-
-        if (this->direction == Direction::North) {
-            this->position.setX((this->position.getX()) - 1);
-        } else if (this->direction == Direction::South) {
-            this->position.setX((this->position.getX()) + 1);
-        } else if (this->direction == Direction::East) {
-            this->position.setY((this->position.getY()) + 1);
-        } else if (this->direction == Direction::West) {
-            this->position.setY((this->position.getY()) - 1);
-        }
-        // increment the travel distance for splitter
-        this->travelDistance +=1;
-        this->path.push_back(this->position);
     }
+    // increment the travel distance for splitter
+    this->travelDistance += 1;
+    this->path.push_back(this->position);
 }
+
 
 Splitter::Splitter(int id, int x, int y, Direction d, int size, int splitFreq, Board *board) {
     this->id = id;
-    Pair p(x,y);
+    Pair p(x, y);
     this->position = p;
     this->direction = d;
     this->size = size;
-    this->splitFrequency=splitFreq;
+    this->splitFrequency = splitFreq;
     this->alive = true;
     this->board = board;
 }
 
 string Splitter::getBugType() {
-   return this->bugType;
+    return this->bugType;
 }
